21dabd5758ce50f77ddbe52a99fe3b18c163a769
Luciano Semicheche <lucianosemicheche@gmail.com>
2021-07-07 23:06:16 -0300

    Add chaching external API request

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import requests_cache
 from sys import path
 from flask import Flask,request
 from flask_restful import Resource, Api
@@ -9,6 +10,7 @@ api = Api(app)
 
 api.add_resource(ConvertDegrees, '/converte', methods=['POST'])
 api.add_resource(SearchPostCode, '/consulta-cep', methods=['GET'])
+requests_cache.install_cache('postmon_cache', backend='sqlite', expire_after=200)
 
 if __name__ == "__main__":
     app.run()
